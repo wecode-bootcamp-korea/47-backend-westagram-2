b257c9fc754ce2a69eb83bd105c8eb512e88f9b9
seongnam <Jseongnam1109@gmail.com>
2023-06-12 21:41:22 +0900

    seongnam

--- app.js
+++ app.js
@@ -17,4 +17,25 @@ myDataSource.initialize()
     .then(() => {
         console.log("Data Source has been initialized!")
     })
+ 
+  console.log("DB_HOST:", process.env.DB_HOST);
+  console.log("DB_USER:", process.env.DB_USERNAME);
+ console.log("DB_PASS:", process.env.DB_PASSWORD);
+  const express = require('express')
+  const logger = require('morgan')
+  const cors = require('cors')
+  const app = express()
+ 
+  app.use(cors())
+  app.use(logger('dev'));
+  app.get('/ping', function (req, res, next) {
+    res.json({message: 'pong'})
+  })
+  app.post('/pingaa', function(req,res,next){
+          res.json({message : 's'});
+  })
+  const port = 3000;
+  app.listen(port, function () {
+    console.log(`server listening on port ${port}`)
+  })
 
