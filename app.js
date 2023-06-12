d588864d1d5c2363a49c1969753a60ba525a60ce
seongnam <Jseongnam1109@gmail.com>
2023-06-12 21:02:10 +0900

    seongnam1

---
+++ app.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const { DataSource } = require('typeorm');
+
+const myDataSource = new DataSource({
+    type: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+})
+
+myDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+
