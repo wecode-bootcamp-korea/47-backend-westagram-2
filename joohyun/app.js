c04869b063f23e9ba7914e8253ede961059d939f
이주현 <leejoohyun@ijuhyeon-ui-MacBookPro.local>
2023-06-14 23:42:07 +0900

    [이주현]Assignment 3,4,5: Express API - CRUD(1)

--- joohyun/app.js
+++ joohyun/app.js
@@ -28,11 +28,36 @@ app.use(cors());
 app.use(logger("combined"));
 app.use(express.json());
 
-app.get("/ping", function (req, res, next) {
+app.get("/ping", function (req, res) {
   res.json({ message: "ping" });
 });
+
+app.post("/users", async function (req, res) {
+  const { name, email, profileImage, password, phoneNumber } = req.body;
+
+  await appDataSource.query(
+    `
+    INSERT INTO users(
+    
+      name,
+      email,
+      profile_image,
+      password,
+      phone_number
+    ) VALUES (
+      ?,
+      ?,
+      ?,
+      ?,
+      ?
+    )
+  `,
+    [name, email, profileImage, password, phoneNumber]
+  );
+  res.status(201).json({ message: "SUCCESS_CREATE_USER" });
+});
+
 app.post("/posts", async function (req, res) {
-  console.log(req.body);
   const { content, user_id } = req.body;
   await appDataSource.query(
     `
@@ -47,10 +72,47 @@ app.post("/posts", async function (req, res) {
     `,
     [content, user_id]
   );
+  res.status(201).json({ message: "SUCCESS_CREATE_POST" });
 });
-res.json({ message: "SUCCESS_CREATE_POST" });
 
+app.get("/guest", async function (req, res) {
+  const guest = await appDataSource.query(`
+      SELECT
+      
+      users.id,
+      users.profile_image AS userProfileImage,
+      posts.id AS postingId,
+      posts.post_image_url AS postingImageUrl,
+      posts.content AS postingContent
+      
+      FROM users, posts
+      WHERE users.id = posts.id
+    `);
+  res.status(200).json({ data: guest });
+});
 
+app.get("/userposts", async function (req, res) {
+  const userposts = await appDataSource.query(`
+      SELECT
+      users.id AS userId,
+      users.profile_image AS userProfileImage,
+      JSON_ARRAY(
+        JSON_OBJECT(
+          'postingId', posts.id,
+          'postingImageUrl', posts.post_image_url,
+          'postingContent', posts.content
+        )
+      ) AS postings
+    FROM
+      users
+    JOIN
+      posts ON users.id = posts.user_id
+    WHERE
+      users.id = 1;
+        `);
+
+  res.status(200).json({ data: userposts });
+});
 app.listen(3000, function () {
   console.log("server listening on port 3000");
 });
