10da1290b04e754d0300e2100a113336b7fd27e3
신동현 <donghyeun02@gmail.com>
2023-06-14 20:42:33 +0900

    ADD : 전체 게시글 조회

--- donghyeun/app.js
+++ donghyeun/app.js
@@ -28,20 +28,6 @@ app.use(cors());
 app.use(logger("combined"));
 app.use(express.json());
 
-app.get("/posts", async (req, res) => {
-  const posts = await appDataSource.query(
-    `
-            SELECT
-                user_id,
-                title,
-                content,
-                imageurl
-            FROM posts
-          `
-  );
-  res.json({ data: posts });
-});
-
 // 게시물 등록
 app.post("/posts", async (req, res) => {
   console.log(req.body);
@@ -64,6 +50,21 @@ app.post("/posts", async (req, res) => {
     [userid, title, content, imageurl]
   );
   res.status(201).json({ message: "postCreated" });
+});
+
+// 전체 게시물 조회
+app.get("/viewAllPosts", async (req, res) => {
+  const viewAllPosts = await appDataSource.query(`
+              SELECT posts.user_id userID
+              , users.profile_image userProfileImage
+              , posts.id postingId
+              , posts.imageurl postingImageUrl
+              , posts.content postingContent
+              FROM users
+              JOIN posts
+              WHERE users.id = posts.user_id;
+          `);
+  res.json({ data: viewAllPosts });
 });
 
 app.listen(3000, function () {
