4265aa34df6931c5a2bd7fec78d5839caff8e5a8
seongnam <Jseongnam1109@gmail.com>
2023-06-15 11:38:29 +0900

    [정성남] Assignment 1:Express API - 초기 환경 세팅

---
+++ seongnam/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require("dotenv")
+dotenv.config()
+
+const { DataSource } = require('typeorm');
+const appDataSource = new DataSource({
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE
+})
+
+appDataSource.initialize()
+    .then(()=>{
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.log("Error during Data Source initialization!",err)
+    appDataSource.destroy();
+    })
+
+const app = express();
+const port = process.env.TYPEORM_PORT;
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.listen(3000, function () {
+    'listening on port 3000'
+    })
