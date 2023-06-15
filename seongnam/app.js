cce2e311a6f442e088cd4abaf559da332fe1fd03
seongnam <Jseongnam1109@gmail.com>
2023-06-15 19:44:11 +0900

    assignment 3,4,5

--- seongnam/app.js
+++ seongnam/app.js
@@ -52,6 +52,71 @@ app.post("/users/signup", async(req,res) =>{
     }
 })
 
+app.post("/write",async(req,res) =>{
+    try{
+        const data = req.body;
+        if(
+            await appDataSource.query(`
+            SELECT EXISTS(SELECT 1 FROM users WHERE id = ?)
+            `,[data.user_id])){
+            await appDataSource.query(`INSERT INTO posts (
+            content,
+            user_id
+            ) 
+        VALUES (
+            ?, 
+            ?
+        )`,
+        [data.content,data.user_id]);
+            res.status(201).json({message : "postCreated"});
+        }else{
+            res.status(201).json({message : "user_id is not exists"});
+        }
+        }catch(error){
+            console.error("Error executing SQL query:", error);
+            res.status(400).json({ message: "Error saving data" });
+        }
+    })
+app.get("/data", async(req,res) =>{   
+    try{
+    const InnerJoinQuery = `
+    SELECT users.id AS userId, 
+    users.profile_image AS userProfileImage, 
+    posts.id AS postingId, posts.postingImageUrl AS postingImageUrl, 
+    posts.content AS postingContent 
+    FROM users 
+    INNER JOIN posts 
+    ON users.id = posts.user_id
+    `
+    const result = await appDataSource.query(InnerJoinQuery);
+    res.status(201).json(result);
+    }catch(error){
+        console.error("Error executing SQL query:", error);
+        res.status(400).json({ message: "Error saving data" });
+    }
+});
+
+app.get("/userdata", async(req,res) => {
+    try{
+        const GroupByJoin = `
+        select users.id, 
+        users.profile_image, 
+        JSON_ARRAYAGG(JSON_OBJECT('postingId', posts.id, 
+        'postingImageUrl', posts.postingImageUrl, 
+        'postingContent', posts.content, 
+        'userId', posts.user_id)) 
+        AS postings
+        FROM users JOIN posts ON users.id = posts.user_id 
+        GROUP BY users.id, users.profile_image;
+        `
+        const result = await appDataSource.query(GroupByJoin);
+        res.status(201).json(result);
+    }catch(error){
+        console.error("Error executing SQL query:", error);
+        res.status(400).json({ message: "Error saving data" });
+    }
+})
+
 app.listen(port, function () {
     `listening on port ${port}`
     })
