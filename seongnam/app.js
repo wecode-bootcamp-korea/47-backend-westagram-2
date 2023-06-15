f5bae03570e90b85bd95867b837b6ad7ffce41c8
seongnam <Jseongnam1109@gmail.com>
2023-06-15 17:59:41 +0900

    ADD:assignment 1 and 2

--- seongnam/app.js
+++ seongnam/app.js
@@ -1,10 +1,12 @@
+const dotenv = require("dotenv")
+dotenv.config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-const dotenv = require("dotenv")
-dotenv.config()
-
 const { DataSource } = require('typeorm');
+
+const app = express();
+
 const appDataSource = new DataSource({
     type: process.env.TYPEORM_CONNECTION,
     host: process.env.TYPEORM_HOST,
@@ -23,11 +25,31 @@ appDataSource.initialize()
     appDataSource.destroy();
     })
 
-const app = express();
 const port = process.env.TYPEORM_PORT;
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.post("/join", async(req,res) =>{
+    try{
+        const data = req.body;
+        console.log(data)
+        const insertQuery = `INSERT INTO users (
+            name,
+            password
+            ) 
+        VALUES (
+            ?, 
+            ?
+        )`;
+        const appData = [ data.name, data.password ];
+        const result = await appDataSource.query(insertQuery, appData);
+        res.status(201).json(result);
+    }catch(error){
+        console.error("Error executing SQL query:", error);
+        res.status(500).json({ message: "Error saving data" });
+    }
+})
 
 app.listen(3000, function () {
     'listening on port 3000'
