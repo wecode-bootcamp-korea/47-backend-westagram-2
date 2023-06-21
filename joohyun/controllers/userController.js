d7dab2587deb17ae0fd6b2b7d3eb668898d73b5e
이주현 <leejoohyun@ijuhyeon-ui-MacBookPro.local>
2023-06-21 14:35:59 +0900

    bcrypt setting

---
+++ joohyun/controllers/userController.js
@@ -0,0 +1,29 @@
+const userService = require("../services/userService");
+
+const signUp = async (req, res) => {
+  try {
+    const { name, email, password, profileImage, phoneNumber } = req.body;
+
+    if (!name || !email || !password || !profileImage || !phoneNumber) {
+      return res.status(400).json({ message: "KEY_ERROR" });
+    }
+
+    await userService.signUp(name, email, password, profileImage, phoneNumber);
+    return res.status(201).json({
+      message: "SIGNUP_SUCCESS",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(err.statusCode || 500).json({ message: err.message });
+  }
+};
+const getUserPosts = async (req, res) => {
+  const { userId } = req.params;
+  const posts = await userService.getUserPosts(userId);
+  return res.status(200).json({ data: posts });
+};
+
+module.exports = {
+  signUp,
+  getUserPosts,
+};
